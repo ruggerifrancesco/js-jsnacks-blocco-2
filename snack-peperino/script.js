a6a71bee575f060696fa70c50c45484f396ccafd
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-30 12:28:33 +0200

    idk what is this

--- snack-peperino/script.js
+++ snack-peperino/script.js
@@ -12,6 +12,8 @@ let totalPeppersWeight = 0;
 addNewPepper('Caroline Repaer', 'Variety 1', 100, 5);
 addNewPepper('Caroline Repaer', 'Variety 2', 120, 6);
 addNewPepper('Ghost Pepper', 'Variety 1', 90, 4);
+addNewPepper('Sweet Pepper', 'Variety 1', 30, 5);
+addNewPepper('Jalapenos', 'Variety 3', 200, 8);
 
 // => Add new Pepper Function Scheme
 function addNewPepper(name, variety, weight, length) {
@@ -31,4 +33,4 @@ for (let i = 0; i < peppersList.length; i++) {
     totalPeppersWeight += peppersList[i].weight;
 }
 
-console.log('Somma totale del peso dei Peperoni:', totalPeppersWeight);
+console.log('Somma totale del peso dei Peperoni:', totalPeppersWeight, 'kg');
